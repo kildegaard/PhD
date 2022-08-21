b4df56733835fdec3a3bb648c24a53e3e1113b78
Gus <gustavo.kildegaard@gmail.com>
2022-08-21 16:15:14 -0300

    Creo y avanzo con parse_espectro.py

---
+++ parse_espectro/parse_espectro.py
@@ -0,0 +1,20 @@
+# parse_espectro.py
+'''
+Programa para parsear los arhivos del espectro del PINMATE
+'''
+
+import sys
+import csv
+from matplotlib import pyplot as plt
+
+
+def parse_espectro():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
