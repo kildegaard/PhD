c4b9ab25a1d5ea0f1462e2873f4f49ae15a6db1a
Gustavo <gustavo.kildegaard@gmail.com>
2022-08-23 13:57:44 -0300

    modificaciones al graficador

--- graficar_espectros/graficar_espectro.py
+++ graficar_espectros/graficar_espectro.py
@@ -1,18 +1,23 @@
 import csv
 import os
 from matplotlib import pyplot as plt
+import pandas as pd
 
 # path = '../parse_espectro/archivos_csv'
 # file_1 = os.listdir(path)[2]
 # path_util = os.path.join(path, file_1)
 # print(path_util)
 
-path_1 = 'C:\\Users\\Gustavo\\Desktop\\cosas lignina\\CurvaCalibLignina\\archivos_csv'
-path_2 = os.listdir(path_1)[0]
-todos_los_path = [os.path.join(path_1, os.listdir(path_1)[i])
-                  for i in range(len(os.listdir(path_1)))]
-path_util = os.path.join(path_1, path_2)
+# path_1 = 'C:\\Users\\Gustavo\\Desktop\\cosas lignina\\CurvaCalibLignina\\archivos_csv'
+# path_2 = os.listdir(path_1)[0]
+# todos_los_path = [os.path.join(path_1, os.listdir(path_1)[i])
+#                   for i in range(len(os.listdir(path_1)))]
+# path_util = os.path.join(path_1, path_2)
 
+os.chdir('archivos_csv')
+todos_los_path = os.listdir()
+print(todos_los_path)
+fig, ax = plt.subplots()
 for path in todos_los_path:
     with open(path, 'rt', encoding='utf-8') as f:
         archivo = csv.reader(f)
@@ -22,6 +27,17 @@ for path in todos_los_path:
             X.append(float(linea[0]))
             Y.append(float(linea[1]))
 
-    fig, ax = plt.subplots()
-    ax.plot(X, Y)
-    plt.show()
+    nombre = f'{path[:-4]} ppm'
+
+    ax.set_ylim([0, 1])
+    ax.set_xlim([309, max(X)])
+
+    ax.grid(True)
+    ax.plot(X, Y, label=nombre)
+    ax.legend(loc='upper right')
+
+ax.text(320, 0.07, '10 ppm')
+ax.text(323, 0.35, '100 ppm')
+ax.text(450, 0.6, '1000 ppm')
+
+plt.show()
